f43262dd04a674cea4368dff6a0d124b20ed3ced
lee233 <32394244+lee233@users.noreply.github.com>
2022-08-07 08:30:56 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ log = logger = logging
 
 class _Config:
     ACT_ID = 'e202009291139501'
-    APP_VERSION = '2.3.0'
+    APP_VERSION = '2.34.1'
     REFERER_URL = 'https://webstatic.mihoyo.com/bbs/event/signin-ys/index.html?' \
                   'bbs_auth_required={}&act_id={}&utm_source={}&utm_medium={}&' \
                   'utm_campaign={}'.format('true', ACT_ID, 'bbs', 'mys', 'icon')
